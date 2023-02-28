93ed0c7d1e628c9faf4ba9a20275202275f86fb9
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-02-28 14:18:29 +0530

    Added Backend

--- Models/Post.model.js
+++ Models/Post.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const postSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { type: String },
   image: { type: String, required: true },
   location: { type: String, required: true },
   postedAt: { type: String, required: true },
